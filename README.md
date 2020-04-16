7a704194296b802b77bc73a636a8c3ad89c095b0
fayzisme <fayzisme@gmail.com>
2020-04-16 22:31:38 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bot Telegram Order Online
+
+## Build Setup
+
+``` bash
+# run server at localhost:8000
+npm run start
+
+# run bot
+node faizal_bot.js
+
+```
--- faizal_bot.js
+++ faizal_bot.js
@@ -1,5 +1,6 @@
 Telegram = require('node-telegram-bot-api')
 require('dotenv').config();
+const axios = require("axios");
 
 const token = process.env.TOKEN_BOT;
 const bot = new Telegram(token, { polling: true });
@@ -11,8 +12,7 @@ bot.onText(/\/start/, (msg) => {
     "reply_markup": {
         "keyboard": [["Panduan"], ["Deskripsi"]]
         }
-    });
-            
+    });        
 }); 
 
 bot.on('message', (msg) => {
@@ -35,4 +35,48 @@ bot.on('message', (msg) => {
         Untuk order product : [/order]
         `, {parse_mode : "Markdown"})
     }
-});       
+}); 
+
+bot.onText(/\/product/, (msg) => {
+    axios.get(`http://localhost:8000/api/product`)
+    .then(response => {
+        const data = response.data.data;
+        console.log(response.data.data);
+        data.forEach(el => {
+            const { id, name, price } = el;
+            const harga = (price) =>
+            {
+                let rupiah = '';		
+                let angkarev = price.toString().split('').reverse().join('');
+                for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+                return 'Rp.'+rupiah.split('',rupiah.length-1).reverse().join('') + ',00';
+            }
+            bot.sendMessage(msg.chat.id, `Nama produk : ${name}
+Harga : ${harga(price)}`);
+        });
+    })
+    .catch(err => {
+        console.log(err.message);
+    });        
+});
+
+bot.onText(/\/order/, async (msg) => {
+    const chatId = msg.chat.id;
+    await bot.sendMessage(chatId, `Kami mengenali anda sebagai,
+Nama : ${msg.from.first_name}, username : ${msg.from.username}
+===================
+Silahkan masukkan Nama Lengkap, Email, dan nomor handphone anda dengan format sebagai berikut:
+    (Nama Lengkap) (email) (nomor handphone)`);
+    bot.sendMessage(chatId, `Contoh :
+    (Fayzisme Faizal) (fayzall@email.com) (0823421234)`);       
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    var message = msg.text.toString();
+    if (message.toLowerCase().indexOf('(') === 0) {
+        const data = [];
+        console.log(msg.text.split(' '));
+        bot.sendMessage(chatId,"Hai");
+    }
+});
