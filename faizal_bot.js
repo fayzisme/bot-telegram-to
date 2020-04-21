6007a92e76f62fd0e7b565b174e5cef0e846aeed
fayzisme <fayzisme@gmail.com>
2020-04-21 16:09:48 +0700

    edit

--- faizal_bot.js
+++ faizal_bot.js
@@ -1,7 +1,6 @@
 Telegram = require('node-telegram-bot-api')
 require('dotenv').config();
 const axios = require("axios");
-
 const token = process.env.TOKEN_BOT;
 const bot = new Telegram(token, { polling: true });
 
@@ -37,28 +36,28 @@ bot.on('message', (msg) => {
     }
 }); 
 
-bot.onText(/\/product/, (msg) => {
-    axios.get(`https://fayzisme-bot-telegram-to.glitch.me/api/product`)
-    .then(response => {
-        const data = response.data.data;
-        console.log(response.data.data);
-        data.forEach(el => {
-            const { id, name, price } = el;
-            const harga = (price) =>
-            {
-                let rupiah = '';		
-                let angkarev = price.toString().split('').reverse().join('');
-                for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
-                return 'Rp.'+rupiah.split('',rupiah.length-1).reverse().join('') + ',00';
-            }
-            bot.sendMessage(msg.chat.id, `Nama produk : ${name}
-Harga : ${harga(price)}`);
-        });
-    })
-    .catch(err => {
-        console.log(err.message);
-    });        
-});
+bot.onText(/\/product/, async (msg) => {
+    try {
+    const response = await axios.get(`https://fayzisme-bot-telegram-to.glitch.me/api/product`)
+    const data = response.data.data;
+    console.log(data);
+         data.forEach(el => {
+             const { id, name, price } = el;
+             const harga = (price) =>
+             {
+                 let rupiah = '';		
+                 let angkarev = price.toString().split('').reverse().join('');
+                 for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+                 return 'Rp.'+rupiah.split('',rupiah.length-1).reverse().join('') + ',00';
+             }
+     bot.sendMessage(msg.chat.id, `Nama produk : ${name}
+ Harga : ${harga(price)}`);
+         })
+    } 
+    catch (err) {
+        console.log(err)
+    }
+});        
 
 bot.onText(/\/order/, async (msg) => {
     const chatId = msg.chat.id;
@@ -71,15 +70,15 @@ Silahkan masukkan Nama Lengkap, Email, dan nomor handphone anda dengan format se
     (Fayzisme Faizal) (fayzall@email.com) (0823421234)`);       
 });
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     var message = msg.text.toString();
     if (message.toLowerCase().indexOf('(') === 0) {
         const username = msg.from.username;
         const [full_name, email, phone_number] = msg.text.split(') ').map(el => el.replace(/[^0-9a-zA-Z@. ]/g, ""))
         console.log(full_name, username, email, phone_number);
-
-       axios.post(`https://fayzisme-bot-telegram-to.glitch.me/api/customers`, 
+    try {
+        const response = await axios.post(`https://fayzisme-bot-telegram-to.glitch.me/api/customers`, 
         {
            data: {
             attributes: {
@@ -90,13 +89,17 @@ bot.on('message', (msg) => {
             }
            } 
         })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        console.log(response.data);
+    }
+    catch (err) {
+        console.log(err);
+    }
 
-        bot.sendMessage(chatId,"Terimakasih data anda sudah terdaftar");
+    bot.sendMessage(chatId,
+`Terimakasih, data anda sudah terdaftar sebagai berikut :
+Nama Lengkap : ${full_name},
+Username : ${username},
+email : ${email},
+Nomor Handphone : ${phone_number}`);
     }
 });
