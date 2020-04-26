b86dc37c475bfbc12c20fd0f359395bf55cddd9e
fayzisme <fayzisme@gmail.com>
2020-04-27 00:15:57 +0700

    menambah callbackQuery

--- faizal_bot.js
+++ faizal_bot.js
@@ -4,7 +4,6 @@ const axios = require("axios");
 const token = process.env.TOKEN_BOT;
 const bot = new Telegram(token, { polling: true });
 
-
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, `Selamat datang ${msg.from.first_name}`, {
@@ -40,34 +39,63 @@ bot.onText(/\/product/, async (msg) => {
     try {
     const response = await axios.get(`https://fayzisme-bot-telegram-to.glitch.me/api/product`)
     const data = response.data.data;
-    console.log(data);
-         data.forEach(el => {
-             const { id, name, price } = el;
-             const harga = (price) =>
-             {
-                 let rupiah = '';		
-                 let angkarev = price.toString().split('').reverse().join('');
-                 for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
-                 return 'Rp.'+rupiah.split('',rupiah.length-1).reverse().join('') + ',00';
-             }
-     bot.sendMessage(msg.chat.id, `Nama produk : ${name}
- Harga : ${harga(price)}`);
-         })
+    data.forEach(el => {
+            const { id, name, price } = el;
+            const harga = (price) =>
+            {
+                let rupiah = '';		
+                let angkarev = price.toString().split('').reverse().join('');
+                for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+                return 'Rp.'+rupiah.split('',rupiah.length-1).reverse().join('') + ',00';
+            }
+    bot.sendMessage(msg.chat.id, `Nama produk : ${name}
+ Harga : ${harga(price)}`,
+ {
+    "reply_markup": {
+        "inline_keyboard": [
+            [
+                {
+                    text: "simpan ke keranjang",
+                    callback_data: id
+                },
+            ],
+        ],
+    }, parse_mode:"Markdown"});
+        });
     } 
     catch (err) {
         console.log(err)
     }
-});        
+});
+
+bot.on("callback_query", function onCallbackQuery(data) {
+    console.log(data);
+});
 
 bot.onText(/\/order/, async (msg) => {
     const chatId = msg.chat.id;
-    await bot.sendMessage(chatId, `Kami mengenali anda sebagai,
-Nama : ${msg.from.first_name}, username : ${msg.from.username}
+    const cek = await axios.get(`https://fayzisme-bot-telegram-to.glitch.me/api/customers/${msg.chat.username}`)
+    const data = cek.data.data;
+
+    if(data === null) { 
+        await bot.sendMessage(chatId, 
+`Data anda belum terdaftar.
 ===================
 Silahkan masukkan Nama Lengkap, Email, dan nomor handphone anda dengan format sebagai berikut:
     (Nama Lengkap) (email) (nomor handphone)`);
-    bot.sendMessage(chatId, `Contoh :
-    (Fayzisme Faizal) (fayzall@email.com) (0823421234)`);       
+        bot.sendMessage(chatId, 
+`Contoh :
+    (Fayzisme Faizal) (fayzall@email.com) (0823421234)`);
+    }
+    else {
+        const {full_name, username, email, phone_number} = data;
+        bot.sendMessage(chatId, 
+`Data anda sudah terdaftar sebagai berikut :
+Nama Lengkap : ${full_name},
+Username : ${username},
+email : ${email},
+Nomor Handphone : ${phone_number}`);
+    }
 });
 
 bot.on('message', async (msg) => {
@@ -77,7 +105,6 @@ bot.on('message', async (msg) => {
         const username = msg.from.username;
         const [full_name, email, phone_number] = msg.text.split(') ').map(el => el.replace(/[^0-9a-zA-Z@. ]/g, ""))
         console.log(full_name, username, email, phone_number);
-    try {
         const response = await axios.post(`https://fayzisme-bot-telegram-to.glitch.me/api/customers`, 
         {
            data: {
@@ -90,16 +117,12 @@ bot.on('message', async (msg) => {
            } 
         })
         console.log(response.data);
-    }
-    catch (err) {
-        console.log(err);
-    }
-
-    bot.sendMessage(chatId,
-`Terimakasih, data anda sudah terdaftar sebagai berikut :
+        bot.sendMessage(chatId, 
+`Data anda sudah terdaftar sebagai berikut :
 Nama Lengkap : ${full_name},
 Username : ${username},
 email : ${email},
 Nomor Handphone : ${phone_number}`);
     }
+
 });
